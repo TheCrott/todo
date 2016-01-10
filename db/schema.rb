46418c578d1bc7c77f81df1ca344f32de21749b7
TheCrott <aldothecrott@gmail.com>
2016-01-10 15:06:06 +0700

    Nambahin user id ke tasks, dan nambahin asosiasi user ke task

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110065540) do
+ActiveRecord::Schema.define(version: 20160110075259) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "context"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
