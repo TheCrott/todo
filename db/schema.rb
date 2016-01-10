0fbb2896b3125031df6ae535224cdf3ccd324cd1
TheCrott <aldothecrott@gmail.com>
2016-01-10 14:03:14 +0700

    Bikin task scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108104313) do
+ActiveRecord::Schema.define(version: 20160110065540) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "context"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
