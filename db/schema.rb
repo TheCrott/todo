534b80ee842d140756a24bfaad27ec53d5fbd8d1
TheCrott <aldothecrott@gmail.com>
2016-01-10 17:00:13 +0700

    Nambahin state ke tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110075259) do
+ActiveRecord::Schema.define(version: 20160110080758) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "context"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "user_id"
+    t.string   "state",      default: "to_do"
   end
 
   create_table "users", force: :cascade do |t|
